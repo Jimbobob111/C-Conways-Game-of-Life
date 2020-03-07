edc465d94d4741ec3bc9f808070af21eafd5f9a8
Robert Thompson <robertpt@hotmail.co.uk>
2020-03-07 11:28:17 +0000

    Upload Code

---
+++ ConwaysGameOfLife/ConwaysGameOfLife/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ConwaysGameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerSetIntBox = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerSetIntBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerSetIntBox
+            // 
+            this.timerSetIntBox.Location = new System.Drawing.Point(13, 219);
+            this.timerSetIntBox.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.timerSetIntBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.timerSetIntBox.Name = "timerSetIntBox";
+            this.timerSetIntBox.Size = new System.Drawing.Size(120, 20);
+            this.timerSetIntBox.TabIndex = 1;
+            this.timerSetIntBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.timerSetIntBox.ValueChanged += new System.EventHandler(this.timerSetIntBox_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Timer Interval (ms)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(218, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 23);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Set Timer Acive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Iterations: xxx";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 297);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timerSetIntBox);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(344, 336);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(344, 336);
+            this.Name = "Form1";
+            this.Text = "Conways Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerSetIntBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown timerSetIntBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConwaysGameOfLife/ConwaysGameOfLife/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConwaysGameOfLife
+{
+    public partial class Form1 : Form
+    {
+        int iterations = 0;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int[,] oldImgArr = CreateOldImgArray();
+            int[,] newImgArr = new int[oldImgArr.GetLength(0), oldImgArr.GetLength(1)];
+            int numAdjacent;
+            for (int x = 0; x < oldImgArr.GetLength(0); x++)
+            {
+                for (int y = 0; y < oldImgArr.GetLength(1); y++)
+                {
+                    numAdjacent = getNumberAjacent(oldImgArr, x, y);
+                    if(numAdjacent==3&&oldImgArr[x,y]==0)
+                    {
+                        newImgArr[x, y] = 1;
+                    }
+                    if(numAdjacent<=1&&oldImgArr[x,y]==1)
+                    {
+                        newImgArr[x, y] = 0;
+                    }
+                    if(numAdjacent>=4&&oldImgArr[x,y]==1)
+                    {
+                        newImgArr[x, y] = 0;
+                    }
+                    if((numAdjacent== 2|| numAdjacent == 3) && oldImgArr[x, y] == 1)
+                    {
+                        newImgArr[x, y] = 1;
+                    }
+                }
+            }
+            pictureBox1.Image = CreateNewImg(newImgArr);
+            iterations++;
+            label2.Text = "Iterations: " + iterations;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Iterations: " + iterations;
+            timerSetIntBox.Value = timer1.Interval;
+            Bitmap img = new Bitmap(200, 200);
+            pictureBox1.Image = img;
+            for (int x=0;x<img.Width;x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    int num = r.Next(0, 100);
+                    if(num<=25)
+                    {
+                        img.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        img.SetPixel(x, y, Color.White);
+                    }
+                    
+                }
+            }
+            checkBox1.Checked = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = checkBox1.Checked;
+        }
+
+        private Bitmap CreateNewImg(int[,] array)
+        {
+            Bitmap img = new Bitmap(array.GetLength(0), array.GetLength(1));
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    if (array[x, y]==1)
+                    {
+                        img.SetPixel(x, y, Color.Black);
+                    }
+                    else
+                    {
+                        img.SetPixel(x, y, Color.White);
+                    }
+
+                }
+            }
+            return img;
+        }
+
+        private int[,] CreateOldImgArray()
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            int[,] array = new int[img.Width, img.Height];
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    if (pixel.ToArgb().Equals(Color.Black.ToArgb()))
+                    {
+                        array[x, y] = 1;
+                    }
+                    else
+                    {
+                        array[x, y] = 0;
+                    }
+                    
+                }
+            }
+            return array;
+        }
+
+        private int getNumberAjacent(int[,] array,int x, int y)
+        {
+            int ajacentAlive=0;
+            if(x!=0)
+            {
+                if(array[x-1,y]==1)
+                {
+                    ajacentAlive++;
+                }
+                if (y + 1 < array.GetLength(1))
+                {
+                    if (array[x - 1, y + 1] == 1)
+                    {
+                        ajacentAlive++;
+                    }
+                }
+            }
+            if (y != 0)
+            {
+                if (array[x, y-1] == 1)
+                {
+                    ajacentAlive++;
+                }
+                if (x + 1 < array.GetLength(0))
+                {
+                    if (array[x + 1, y - 1] == 1)
+                    {
+                        ajacentAlive++;
+                    }
+                }
+            }
+            if(x!=0&&y!=0)
+            {
+                if (array[x - 1, y - 1] == 1)
+                {
+                    ajacentAlive++;
+                }
+            }
+            if(y+1<array.GetLength(1))
+            {
+                if (array[x, y + 1] == 1)
+                {
+                    ajacentAlive++;
+                }
+                if (x + 1 < array.GetLength(0))
+                {
+                    if (array[x + 1, y+1] == 1)
+                    {
+                        ajacentAlive++;
+                    }
+                }
+            }
+            if(x+1<array.GetLength(0))
+            {
+                if (array[x + 1, y] == 1)
+                {
+                    ajacentAlive++;
+                }
+            }
+            return ajacentAlive;
+        }
+
+        private void timerSetIntBox_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(timerSetIntBox.Value);
+        }
+    }
+}
